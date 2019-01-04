54b1693a959dd6c0fa4f1dc4dd0328b33d79f03e
Maciek <maciej.moroz89@gmail.com>
2019-01-04 20:48:04 +0100

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,32 @@ var autoprefixer = require("gulp-autoprefixer");
 var browserSync = require("browser-sync").create();
 var nunjacksRender = require("gulp-nunjucks-render");
 
-gulp.task("img", function() {
+gulp.task("img", function(done) {
   gulp.src("./src/img/**/*").pipe(gulp.dest("./web/img"));
+  done();
 });
 
-gulp.task("fonts", function() {
+gulp.task("fonts", function(done) {
   gulp.src("./src/fonts/**/*").pipe(gulp.dest("./web/fonts"));
+  done();
 });
 
-gulp.task("sass", function() {
+gulp.task("sass", function(done) {
   gulp
     .src("./src/scss/style.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest("./web/css/"))
     .pipe(browserSync.stream());
+  done();
 });
 
-gulp.task("js", function() {
+gulp.task("js", function(done) {
   gulp.src(["src/js/*"]).pipe(gulp.dest("./web/js/"));
+  done();
 });
 
-gulp.task("nunjucks", function() {
+gulp.task("nunjucks", function(done) {
   return gulp
     .src(["src/templates/*.html"])
     .pipe(
@@ -36,21 +40,27 @@ gulp.task("nunjucks", function() {
       })
     )
     .pipe(gulp.dest("./web"));
+  done();
 });
 
-gulp.task("build", ["img", "fonts", "sass", "nunjucks", "js"], function() {
-  browserSync.init({
-    server: "./web"
-  });
-  gulp.watch("src/img/**/*", ["img"]);
-  gulp.watch("src/fonts/**/*", ["fonts"]);
-  gulp.watch("src/scss/**/*.scss", ["sass"]);
-  gulp.watch("src/templates/**/*.html", ["nunjucks"]);
-  gulp.watch("src/js/*.js", ["js"]);
-  gulp.watch("web/*.html").on("change", browserSync.reload);
-  gulp.watch("web/js/*.js").on("change", browserSync.reload);
-  gulp.watch("web/img/**/*").on("change", browserSync.reload);
-  gulp.watch("web/fonts/**/*").on("change", browserSync.reload);
-});
+gulp.task(
+  "build",
+  gulp.series(["img", "fonts", "sass", "nunjucks", "js"], function() {
+    browserSync.init({
+      server: "./web"
+    });
+    gulp.watch("src/img/**/*", gulp.parallel(["img"]));
 
-gulp.task("default", ["build"]);
+    gulp.watch("src/fonts/**/*", gulp.parallel(["fonts"]));
+    gulp.watch("src/scss/**/*.scss", gulp.parallel(["sass"]));
+    gulp.watch("scss/**/*.scss", gulp.parallel(["sass"]));
+    gulp.watch("src/templates/**/*.html", gulp.parallel(["nunjucks"]));
+    gulp.watch("src/js/*.js", gulp.parallel(["js"]));
+    gulp.watch("web/*.html").on("change", browserSync.reload);
+    gulp.watch("web/js/*.js").on("change", browserSync.reload);
+    gulp.watch("web/img/**/*").on("change", browserSync.reload);
+    gulp.watch("web/fonts/**/*").on("change", browserSync.reload);
+  })
+);
+
+gulp.task("default", gulp.series(["build"]));
