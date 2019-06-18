587534075cb3ecc316a8cc41b3cd829f6ade7749
Maciej Moroz <maciej.moroz89@gmail.com>
2019-06-18 11:45:39 +0200

    colapse fix

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
+    console.log(this.classList);
     var content = this.nextElementSibling;
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
