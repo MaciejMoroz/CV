5aee964d88447220c6e869f8e642d3e731e1a24a
Maciej Moroz <maciej.moroz89@gmail.com>
2019-08-05 21:03:23 +0200

    add ig

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
-var coll = document.getElementsByClassName("colapse");
-var i;
+const coll = document.getElementsByClassName("colapse");
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    console.log(this.classList);
-    var content = this.nextElementSibling;
+    const content = this.nextElementSibling;
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
